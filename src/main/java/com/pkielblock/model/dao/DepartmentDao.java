87b241cd74451f68f8037972a3e1e692532096cc
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-24 15:26:23 -0300

    DAOs

---
+++ src/main/java/com/pkielblock/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package com.pkielblock.model.dao;
+
+import com.pkielblock.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
---
+++ src/main/java/com/pkielblock/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package com.pkielblock.model.dao;
+
+import com.pkielblock.model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
