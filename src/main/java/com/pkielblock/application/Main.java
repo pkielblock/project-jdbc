6ac07805a0a4c4c49b30c57ef29589d180814a9e
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-24 15:22:28 -0300

    Seller Class

--- src/main/java/com/pkielblock/application/Main.java
+++ src/main/java/com/pkielblock/application/Main.java
@@ -1,11 +1,15 @@
 package com.pkielblock.application;
 
 import com.pkielblock.model.entities.Department;
+import com.pkielblock.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(1, "Pedro", "pedro@gmail.com", new Date(), 3000.0, department);
 
-        System.out.println(obj);
+        System.out.println(seller);
     }
 }
---
+++ src/main/java/com/pkielblock/model/entities/Seller.java
@@ -0,0 +1,99 @@
+package com.pkielblock.model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date bithDate;
+    private Double baseSalary;
+    private Department department;
+
+    public Seller() {
+
+    }
+
+    public Seller(Integer id, String name, String email, Date bithDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.bithDate = bithDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBithDate() {
+        return bithDate;
+    }
+
+    public void setBithDate(Date bithDate) {
+        this.bithDate = bithDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Seller seller)) return false;
+        return getId().equals(seller.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", bithDate=" + bithDate +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department +
+                '}';
+    }
+}
